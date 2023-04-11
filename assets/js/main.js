f91ff5562cf39dfcaf08c60fdba99b83a6ddfb01
Fitim Tahiraj <fitimtahiraj@fitims-air.myfiosgateway.com>
2023-04-10 23:28:22 -0400

    Made buttons to work

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,33 +2,33 @@
 
 var questions = [{
     question: "Commonly used data types DO not include:",
-    choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
-    correct: "3. alerts",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    correct: "alerts",
 },
 {
     question: "The condition in an if/else statement is enclosed within _____.",
-    choices: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
+    choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
     correct: "4. parenthesis", 
 },
 {
     question: "Arrays in javascript can be used to store _______.",
-    choices: ["1. number and strings", "2. other arrays", "3. booleans", "4. all of the above"],
-    correct: "4. all of the above", 
+    choices: ["number and strings", "other arrays", "booleans", "all of the above"],
+    correct: "all of the above", 
 },
 {
     question: "Inside which HTML element do we put the JavaScript?",
-    choices: ["1. <scripting>", "2. <script>", "3. <js>", "4. <javascript>"],
-    correct: "2. <script>", 
+    choices: ["<scripting>", "<script>", "<js>", "<javascript>"],
+    correct: "<script>", 
 },
 {
     question: "Where is the correct place to insert a JavaScript?",
-    choices: ["1. body", "2. head", "3. both", "4. neither"],
-    correct: "3. both", 
+    choices: ["body", "head", "both", "neither"],
+    correct: "both", 
 },
 {
     question: "How does a FOR loop start?",
-    choices: ["1. for(i = 0; i <= 50)", "2. for (i = 0;i <= 5; i++)", "3. for (i <= 5; i++)", "4. for i = 1 to 5"],
-    correct: "3. for (i = 0; i <= 5; i++)", 
+    choices: ["1. for(i = 0; i <= 50)", "2. for (i = 0; i <= 5; i++)", "3. for (i <= 5; i++)", "4. for i = 1 to 5"],
+    correct: "2. for (i = 0; i <= 5; i++)", 
 },
 ]
 
@@ -57,18 +57,18 @@ var optionBtn = document.querySelector(".option-buttons")
 
 //Questions in quiz-data section
 var quizQuestions = document.querySelector("#quiz-questions");
-var answerButton = document.querySelectorAll(".button");
+// var answerButton = document.querySelectorAll(".button");
 var rightAnswer = document.querySelector("#right-answer");
 var wrongAnswer = document.querySelector("#wrong-answer");
 
 //Submitted answers
-var finalScore = document.querySelector("#final-score");
+var finalScore = document.querySelector(".final-score");
 var scoreNumber = document.querySelector("#score-number");
 
 //Time countdown variables
-var setTime;
 var secondsLeft = 60;
 var liveScore = 0;
+var Index = 0;
 
 
 //Quiz starts here
@@ -79,73 +79,72 @@ function beginQuiz () {
     quizWelc.style.display = "none";
     quizData.style.display = "flex";
 
-    //Add event listener to buttons to check the if its right or wrong.
-    for (var i = 0; i < button.length; i++) {
-        button[i].addEventListener("click",checkAnswer);
-    }
+    timeCountdown.textContent = "Time: " +secondsLeft;
 
     //Start countdown
-    setTime = setInterval(function() {
+    var setTime = setInterval(function() {
         if(secondsLeft > 0) {
             secondsLeft--;
             timeCountdown.textContent = "Time: " + secondsLeft;
         } else {
+            clearInterval(setTime);
             completedQuiz();
         }
     }, 1000);
-
-    
 };
 
+var correctAswer = questions[Index].correct;
 
-var correctAnswer = questions.map(({questions, choices, correct}) => ({correct}));
-var ans = answer.textContent
-function checkAnswer() {
-    if (ans === correctAnswer) {
-     secondsLeft = secondsLeft + 10;
+      //Validate answers
+      for (let i = 0; i < button.length; i++) {
+        button[i].addEventListener("click", checkAnswer);}
+    
+    
+
+function checkAnswer(event) {
+    var x = event.target.children[1].textContent;
+    if (x === correctAswer) {
+        liveScore +=30;
+        finalScore.textContent = liveScore;
+        rightAnswer.style.display = "flex";
+        wrongAnswer.style.display = "none";
     } else {
-    secondsLeft = secondsLeft - 10;
+        liveScore -= 10;
+        secondsLeft -=10;
+        rightAnswer.style.display = "none";
+        wrongAnswer.style.display ="flex";
     }
+    setTimeout(() => {
+        Index++;
+        resQuiz();
+    }, 700);
 }
 
+function resQuiz() {
+    rightAnswer.style.display = "none";
+    wrongAnswer.style.display = "none";
+
+    if (secondsLeft > 0 && Index < 6) {
+        for (var i = 1; i < questions.length; i++) {
+            if (Index === i) {
+                quizQuestions.textContent = questions[i].question;
+                let a = questions[i];
+                for (var i = 0; i < a.choices.length; i++){
+                answer[i].textContent = a.choices[i];
+                }
+                }
+            }
+        }
+    }
 
 
-//Questions loop
-var textIndex = 1;
-optionBtn.addEventListener("click", contQuiz)
-
-function contQuiz() {
-
-
-    if (secondsLeft > 0 && liveScore < 10) {
-        if(textIndex < questions.length) {
-            quizQuestions.textContent = questions[textIndex].question;
-            textIndex++;
-            var x = questions[textIndex - 1]
-            for (var i = 0; i < x.choices.length; i++) {
-                answer[i].textContent = x.choices[i];
-                      }
-              } else {
-                // secondsLeft = 0;
-                // completedQuiz();
-                // liveScore += secondsLeft;
-              }
-        } 
-            
-        };
 
 function completedQuiz() {
     quizData.style.display = "none";
     quizDone.style.display = "flex";
-    
-    submitHighScore.addEventListener("click", viewHighScores);
 }
+console.log("here");
 
-
-function viewHighScores() {
-    quizDone.style.display = "none";
-    quizScore.style.display = "flex";
-};
 
 
 
